b46a0d8a072a269b8340199f5a7e9a0daca61d57
Sun Jia Cheng <sunjc826@gmail.com>
2022-03-09 22:58:40 +0800

    add stack

---
+++ elementary_data_structures/stack/stack_1.c
@@ -0,0 +1,30 @@
+#define MAXSIZE 100
+
+typedef int item_t;
+typedef struct
+{
+    int size;
+    item_t stack[MAXSIZE];
+} ArrayStack;
+
+int stack_empty(ArrayStack *arr_stack)
+{
+    return (arr_stack->size == 0);
+}
+
+int push(ArrayStack *arr_stack, item_t item)
+{
+    if (arr_stack >= MAXSIZE)
+    {
+        return -1;
+    }
+    arr_stack->stack[arr_stack->size] = item;
+    arr_stack->size++;
+    return -1;
+}
+
+item_t pop(ArrayStack *arr_stack)
+{
+    arr_stack->size--;
+    return arr_stack->stack[arr_stack->size];
+}
---
+++ elementary_data_structures/stack/stack_2.c
@@ -0,0 +1,57 @@
+typedef int item_t;
+typedef struct
+{
+    item_t *base;
+    // top is always 1 position above the top element
+    // In particular, when the stack is empty, top == base
+    item_t *top;
+    int capacity;
+} PointerStack;
+
+PointerStack *create_stack(int capacity)
+{
+    // PointerStack is still a contiguous array.
+    PointerStack *ps = (PointerStack *)malloc(sizeof(PointerStack));
+    ps->base = (item_t *)malloc(capacity * sizeof(item_t));
+    ps->capacity = capacity;
+    ps->top = ps->base;
+    return ps;
+}
+
+int stack_empty(PointerStack *ps)
+{
+    // Note: we are comparing item_t* with item_t*, so this is
+    // an address comparison.
+    return (ps->base == ps->top);
+}
+
+int push(PointerStack *ps, item_t item)
+{
+    if (ps->top >= ps->base + ps->capacity)
+    {
+        // stack is full
+        return -1;
+    }
+
+    *(ps->top) = item;
+    ps->top += 1;
+    return 0;
+}
+
+item_t pop(PointerStack *ps)
+{
+    ps->top -= 1;
+    return *(ps->top);
+}
+
+// peek
+item_t top_element(PointerStack *ps)
+{
+    return *(ps->top - 1);
+}
+
+void remove_stack(PointerStack *ps)
+{
+    free(ps->base);
+    free(ps);
+}
