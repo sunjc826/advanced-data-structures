b6ec0d20ec043806d0b248e67a0ba0d2b562e4f6
sunjc826 <sunjc826@gmail.com>
2022-03-20 12:10:09 +0800

    add ab-tree insert

--- balanced_search_trees/a_b_B_trees.md
+++ balanced_search_trees/a_b_B_trees.md
@@ -1,3 +1,7 @@
+**Discussion** A B-tree or an (a,b)-tree both stores a single (large) node in a (possibly external) memory block. In comparison, if a regular binary tree were to be persisted to external memory, a memory block would likely contain many different nodes. Furthermore, it may not be the case that the many different nodes in a memory block are in the vicinity of our search path down a binary tree. Hence, a single search of a binary tree may require fetching many (logarithmic in the best case) memory blocks, for e.g. in the case that each memory block does not contain the immediate descendants of the current node along the search path. Hence, a single large external memory block may only provide the search operation with a single useful node, which is small, causing inefficiency.
+
+In contrast, as a memory block of a B-tree or (a,b)-tree contains 1 (very big) node, having a very large branch factor of at least $a$, a single read from external memory allows us to narrow down the search by a larger factor. (e.g. in a binary search, an interior node reduces the search space by about $\frac{1}{2}$, but a B/(a,b)-tree node reduces the search space by around $\frac{1}{a}$, where $a$ is much larger than 2.) This means that a single fetch from external memory does more work.
+
 **Proposition** For an (a,b)-tree of height $h\geq 1$ where $n$ denotes the number of leaves, $2a^{h-1}\leq n\leq b^h$.
 The lower bound on n, $2a^{h-1}\leq n$ derives an upper bound for $h$ given $n$: $h\leq \log_a(n) - \log_a(2) + 1 = \frac{\log n}{\log a} - \log_a(2) + 1$
 
@@ -113,3 +117,41 @@ $$
 where $lower + 1 < upper$ (since $upper <= lower + 1$ is the termination condition, as $[lower, lower + 1)$ represents a singleton $\{lower\}$).
 - This is guaranteed to terminate since $lower + 1 < upper$ implies that $lower< mid <upper$ so $[lower, mid), [mid, upper)\subsetneq [lower, upper)$.
 
+
+**Proposition** In the long run, each insert/delete can on average cause at most $\frac{3}{2}$ restructuring operations plus possibly one root operation.
+Denote
+- $T_i$ as a tree after the $i$-th insertion/deletion (plus any additional restructuring) at the lowest level, in particular, $T_0$ is the initial tree.
+- $V_i$ as the set of vertices of $T_i$
+- $n$ as a node
+- $\phi$ as the potential function. $\phi(n)$ is the potential of a tree node $n$. $\phi(T_i)$ is the potential of the tree after the $i$-th insertion/deletion. Note that $\phi(T_i) = \sum_{n\in V_i}\phi(n)$
+- $M_N$ as the average number of restructuring operations caused by $N$ insertions/deletions. 
+We use the following lemmas:
+- $0\leq \phi(T_0)\leq 6|V_0|$
+- Each insertion/deletion causes at the lowest level increases the tree potential by at most $3$.
+- Each restructuring operation decreases the tree potential by at least $2$.
+
+By definition of $\phi(n)$, at any step, $\forall n\in V_i, \phi(n)\geq 0$ so that $\phi(T_i)\geq 0$. Also, for any $i$, $\Delta \phi(T_i) \leq 3 - 2\cdot \text{m_i}$ where $m_i$ is the number of restructuring operations caused by the ith insertion/deletion. Note that $M_N = \frac{1}{N}\sum_{1\leq i\leq N}m_i$.
+
+Hence, the following inequality holds for all $N\in \mathbb{Z}^+$:
+$$
+    6|V_0| + (3N - 2M_NN) \geq \phi(T_0) + (\phi(T_n) - \phi(T_0)) = \phi(T_n) \geq 0
+$$
+Hence, $M_N\leq \frac{3|V_0|}{N} + \frac{3}{2}$, and taking limits gives $\lim_{N\rightarrow\infty}M_N\leq \frac{3}{2}$. $\square$
+
+**Discussion** Insertion in a non-leaf node of an (a,b)-tree
+Let $n$ be a non-leaf node with search space $[A, B]$ for some $A, B$. Let $key$ be the `key` array of $n$. This array partitions the search space $[A, B]$ into
+$$
+[A, key[1]), [key[1], key[2]), \dots [key[key.length - 1], B)
+$$
+Important! Note that we have no guarantee that $key[0] = A$. So in other words, if we are only given node $n$ and its descendants, we would not know what $A$ actually is.
+
+Consider what happens if we need to insert a child node $c$ under $n$, given that the search space of $c$ is $[j, k)\subseteq [A, B)$ and that we are guaranteed that $[j, k)$ either
+- Lies between the child nodes $[k_1, k_2), [k_2, k_3)$ such that $k \leq k_2$
+  In this case, we simply can do this: $[k_1, j), [j, k_2), [k_2, k_3)$
+- Lies after the last child node, i.e. $[k_1, k_2)$ such that $k\leq k_2$
+  In this case, we can do this: $[k_1, j), [j, k)$.
+In either case, we are partitioning the search space $[k_1, k_2)$ into $[k_1, j), [j, k_2)$. In code terms, we only need to change 1 key value in the `key` array, i.e. set it to $j$, which is known.
+
+*In other words*, if we can guarantee that $[j, k)$ is in the upper end of some $[k_1, k_2)$, we can easily insert $[j, k)$ by
+- inserting $j$ into the `key` array *before* $k_2$ and,
+- inserting the node representing $[j, k_2)$ *after* the node representing $[k_1, j)$. In particular, $[j, k_2)$ will not be the first node.
--- balanced_search_trees/a_b_tree.c
+++ balanced_search_trees/a_b_tree.c
@@ -10,6 +10,24 @@ typedef struct tr_n_t
     struct tr_n_t *next[B];
 } TreeNode;
 
+TreeNode *get_node();
+void return_node(TreeNode *);
+
+void create_stack();
+int stack_empty();
+int push(TreeNode *);
+TreeNode *pop();
+void remove_stack();
+
+TreeNode *create_tree()
+{
+    TreeNode *temp;
+    temp = get_node();
+    temp->height = 0;
+    temp->degree = 0;
+    return temp;
+}
+
 object_t *find(TreeNode *tree, key_t query_key)
 {
     TreeNode *current_node;
@@ -50,4 +68,153 @@ object_t *find(TreeNode *tree, key_t query_key)
             return object;
         }
     }
+}
+
+int insert(TreeNode *tree, key_t new_key, object_t *new_object)
+{
+    TreeNode *current_node;
+    TreeNode *insert_pt;
+    key_t insert_key;
+    int finished;
+    current_node = tree;
+    if (tree->height == 0 && tree->degree == 0)
+    {
+        tree->key[0] = new_key;
+        tree->next[0] = (TreeNode *)new_object;
+        tree->degree = 1;
+        return 0;
+    }
+    create_stack();
+    while (current_node->height > 0)
+    {
+        int lower;
+        int upper;
+        push(current_node);
+        lower = 0;
+        upper = current_node->degree;
+        while (lower + 1 < upper)
+        {
+            if (new_key < current_node->key[(lower + upper) / 2])
+            {
+                upper = (lower + upper) / 2;
+            }
+            else
+            {
+                lower = (lower + upper) / 2;
+            }
+        }
+        current_node = current_node->next[lower];
+    }
+
+    insert_pt = (TreeNode *)new_object;
+    insert_key = new_key;
+    finished = 0;
+    while (!finished)
+    {
+        int i;
+        int start; // index of lowest key
+        if (current_node->height > 0)
+        {
+            start = 1; // non-leaf node keys lie in indices 1 to B-1
+        }
+        else
+        {
+            start = 0; // leaf keys lie in indices 0 to B-1
+        }
+        // case (a)
+        if (current_node->degree < B)
+        {
+            i = current_node->degree;
+            // this is similar to insertion sort's inner loop
+            // notice that for non-leaf nodes, insertion at index 0 never occurs
+            // to show that this is correct, we need to consider all cases where we would need to insert inner node, i.e. splitting, sharing, joining
+            // for splitting, see the comment below: we always insert the upper half of a split node, so the upper half `->next` index >= 1
+            while ((i > start) && (current_node->key[i - 1] > insert_key))
+            {
+                current_node->key[i] = current_node->key[i - 1];
+                current_node->next[i] = current_node->next[i - 1];
+                i -= 1;
+            }
+            current_node->key[i] = insert_key;
+            current_node->next[i] = insert_pt;
+            current_node->degree += 1;
+            finished = 1;
+        }
+        // case (b): Overfull, hence need to conduct operation: split
+        else
+        {
+            TreeNode *new_node;
+            int j;
+            int insert_done = 0;
+            new_node = get_node();
+            i = B - 1;
+            j = (B - 1) / 2;
+            while (j >= 0)
+            {
+                if (insert_done || insert_key < current_node->key[i])
+                {
+                    new_node->next[j] = current_node->next[i];
+                    new_node->key[j] = current_node->key[i];
+                    j--;
+                    i--;
+                }
+                else
+                {
+                    new_node->next[j] = insert_pt;
+                    new_node->key[j] = insert_key;
+                    j--;
+                    insert_done = 1;
+                }
+            }
+            // upper half has been moved out, but no insertion was made
+            // i.e. insertion needs to be done in lower half
+            while (!insert_done)
+            {
+                if (insert_key < current_node->key[i] && i >= start)
+                {
+                    current_node->next[i + 1] = current_node->next[i];
+                    current_node->key[i + 1] = current_node->key[i];
+                    i--;
+                }
+                else
+                {
+                    current_node->next[i + 1] = insert_pt;
+                    current_node->key[i + 1] = insert_key;
+                    insert_done = 1;
+                }
+            }
+
+            current_node->degree = B + 1 - ((B + 1) / 2); // ceiling
+            new_node->degree = (B + 1) / 2;
+            new_node->height = current_node->height;
+            // we always try to insert the upper half, so that its associated next index in the parent node >= 1.
+            insert_pt = new_node;
+            insert_key = new_node->key[0];
+            // not splitting root
+            if (!stack_empty())
+            {
+                current_node = pop();
+            }
+            // splitting root
+            else
+            {
+                new_node = get_node();
+                for (i = 0; i < current_node->degree; i++)
+                {
+                    new_node->next[i] = current_node->next[i];
+                    new_node->key[i] = current_node->key[i];
+                }
+                new_node->height = current_node->height;
+                new_node->degree = current_node->degree;
+                current_node->height += 1;
+                current_node->degree = 2;
+                current_node->next[0] = new_node;
+                current_node->next[1] = insert_pt;
+                current_node->key[1] = insert_key;
+                finished = 1;
+            }
+        }
+    }
+    remove_stack();
+    return 0;
 }
